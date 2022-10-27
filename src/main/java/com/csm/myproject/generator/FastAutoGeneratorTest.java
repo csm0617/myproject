56655a621afef08d4c5dfb6931e047fe0bde6c02
陈水明 <439752552@qq.com>
2022-10-27 10:02:57 +0800

    在MybatisPlus的自动生成配置上，builder.disable增加了禁用模板，用addExclude通过排除空表来生成全部的表,避免手动输入全部的表

--- src/main/java/com/csm/myproject/generator/FastAutoGeneratorTest.java
+++ src/main/java/com/csm/myproject/generator/FastAutoGeneratorTest.java
@@ -1,6 +1,7 @@
 package com.csm.myproject.generator;
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.TemplateType;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.util.Collections;
@@ -15,7 +16,7 @@ public class FastAutoGeneratorTest {
         FastAutoGenerator.create("jdbc:mysql://127.0.0.1:3306/mydb?characterEncoding=utf-8&userSSL=false", "root", "123456")
                 .globalConfig(builder -> {
                     builder.author("csm") // 设置作者
-                            //.enableSwagger() // 开启 swagger 模式
+                            .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
                             .outputDir("src/main/java"); // 指定输出目录
                 })
@@ -25,10 +26,17 @@ public class FastAutoGeneratorTest {
                             .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "src/main/resources/com/csm/myproject/mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("m_user","m_role","m_user_role","m_role_menu","m_menu","m_fir_menu","m_sec_menu","m_menu_firmenu","m_firmenu_secmenu") // 设置需要生成的表名
+                    //用addExclude通过排除空表来生成全部的表
+                    builder.addExclude("")
+                            //.addInclude("m_user","m_role","m_user_role","m_role_menu","m_menu","m_fir_menu","m_sec_menu","m_menu_firmenu","m_firmenu_secmenu") // 设置需要生成的表名
                             .addTablePrefix("m_", "t_"); // 设置过滤表前缀
                 })
-                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .templateEngine(new FreemarkerTemplateEngine())
+                //禁用模板生成哪些模板
+                .templateConfig(builder -> {
+                    builder.disable(TemplateType.CONTROLLER,TemplateType.SERVICEIMPL,TemplateType.SERVICE,TemplateType.MAPPER);
+                }) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
     }
 }
